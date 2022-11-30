0d6ec58b79f1000b1802f8edf73d11ea48add779
Nicholas <nskins@umich.edu>
2022-11-30 18:45:45 -0500

    Move exercise seed data to standard Ecto migration

--- priv/repo/migrations/20221130232025_add_default_exercises.exs
+++ priv/repo/migrations/20221130232025_add_default_exercises.exs
@@ -1,7 +1,57 @@
 defmodule WeLift.Repo.Migrations.AddDefaultExercises do
   use Ecto.Migration
+  alias WeLift.Workouts
 
-  def change do
+  def up do
+    for exercise <- default_exercises do
+      Workouts.upsert_exercise(%{name: exercise})
+    end
+  end
 
+  def down do
+    for exercise <- default_exercises do
+      Workouts.delete_exercise(exercise)
+    end
+  end
+
+  defp default_exercises do
+    [
+      "Band Roundhouse Elbow",
+      "Barbell Curl",
+      "Bench Press",
+      "Cable Crossover",
+      "Cable Crunch",
+      "Cable Oblique Crunch",
+      "Cable Overhead Triceps Extension",
+      "Cable Woodchopper",
+      "Crunch",
+      "Dumbbell Bent-Over Row",
+      "Dumbbell Incline Curl",
+      "Dumbbell Lateral Raise",
+      "Dumbbell Side Bend",
+      "Dumbbell Shoulder Press",
+      "Dumbbell Shrug",
+      "Hanging Leg Raise",
+      "High Cable Rear Delt Fly",
+      "Hip Thrust",
+      "Incline Bench Press",
+      "Incline Dumbbell Flye",
+      "Leg Extension",
+      "Leg Press Calf Raise",
+      "Lying Leg Curl",
+      "Lying Triceps Extension",
+      "Oblique Crunch",
+      "One-Arm Cable Front Raise",
+      "One-Arm High Cable Curl",
+      "One-Leg Leg Press",
+      "Romanian Deadlift",
+      "Seated Calf Raise",
+      "Squat",
+      "Standing Calf Raise",
+      "Standing Pulldown",
+      "Straight Arm Pulldown",
+      "Triceps Pressdown",
+      "Wide-Grip Pulldown"
+    ]
   end
 end
