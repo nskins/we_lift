bbf2bbad5c61b67bf5adbc1ffdc7a4212b7660da
Nicholas <nskins@umich.edu>
2022-12-01 20:35:55 -0500

    Add a second set of default exercises in new migration (#19)
    * Run 'mix ecto.gen.migration add_more_default_exercises'
    
    * Add a second set of default exercises in new migration
    
    * Add reverse machine flye exercise

---
+++ priv/repo/migrations/20221201233858_add_more_default_exercises.exs
@@ -0,0 +1,45 @@
+defmodule WeLift.Repo.Migrations.AddMoreDefaultExercises do
+  use Ecto.Migration
+  alias WeLift.Workouts
+
+  def up do
+    for exercise <- default_exercises() do
+      Workouts.upsert_exercise(%{name: exercise})
+    end
+  end
+
+  def down do
+    for exercise <- default_exercises() do
+      Workouts.delete_exercise(exercise)
+    end
+  end
+
+  defp default_exercises do
+    [
+      "Barbell Shrug",
+      "Behind-Back Cable Curl",
+      "Behind-Neck Pulldown",
+      "Bent-Over Lateral Raise",
+      "Cable Lying Triceps Extension",
+      "Close-Grip Bench Press",
+      "Dumbbell Concentration Curl",
+      "Dumbbell Flye",
+      "Dumbbell Overhead Triceps Extension",
+      "Dumbbell Upright Row",
+      "Front Squat",
+      "Incline Cable Curl",
+      "Incline Cable Flye",
+      "Leg Press",
+      "One-Arm Cable Lateral Raise",
+      "One-Arm Smith Machine Shrug",
+      "One-Arm Overhead Cable Triceps Extension",
+      "Preacher Curl",
+      "Reverse-Grip Incline Dumbbell Press",
+      "Reverse-Grip Pulldown",
+      "Reverse Machine Flye",
+      "Seated Cable Row",
+      "Seated Leg Curl",
+      "Smith Machine Upright Row"
+    ]
+  end
+end
