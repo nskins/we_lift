67e4d8563afce2242c78c4f5fae9eba2b787eeac
Nicholas <nskins@umich.edu>
2022-12-04 16:55:29 -0500

    Order exercises alphabetically on edit workout page

--- lib/we_lift/sort.ex
+++ lib/we_lift/sort.ex
@@ -1,4 +1,8 @@
 defmodule WeLift.Sort do
+  def alphabetically(list, field_selector) do
+    Enum.sort(list, &(field_selector.(&1) <= field_selector.(&2)))
+  end
+
   def chronologically(list) do
     Enum.sort(list, &(&1.inserted_at <= &2.inserted_at))
   end
