49edbd69127fe7234209e5d047580c7616255251
Nicholas <nskins@umich.edu>
2022-12-16 16:10:32 -0500

    Remove margin from top of simple form component

--- lib/we_lift_web/components/core_components.ex
+++ lib/we_lift_web/components/core_components.ex
@@ -191,7 +191,7 @@ defmodule WeLiftWeb.CoreComponents do
   def simple_form(assigns) do
     ~H"""
     <.form :let={f} for={@for} as={@as} {@rest}>
-      <div class="space-y-8 bg-white mt-10">
+      <div class="space-y-8 bg-white">
         <%= render_slot(@inner_block, f) %>
         <div :for={action <- @actions} class="mt-2 flex items-center justify-between gap-6">
           <%= render_slot(action, f) %>
