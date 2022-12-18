ca072afc8a0a3ba9580bdad11d3b034778df52ac
Nicholas <nskins@umich.edu>
2022-12-18 12:56:54 -0500

    Implement migration for adding batch 3 of exercises

--- priv/repo/migrations/20221218172141_add_exercises_batch_3.exs
+++ priv/repo/migrations/20221218172141_add_exercises_batch_3.exs
@@ -1,7 +1,30 @@
 defmodule WeLift.Repo.Migrations.AddExercisesBatch3 do
   use Ecto.Migration
+  alias WeLift.Workouts
 
-  def change do
+  def up do
+    for exercise <- default_exercises() do
+      Workouts.upsert_exercise(%{name: exercise})
+    end
+  end
 
+  def down do
+    for exercise <- default_exercises() do
+      Workouts.delete_exercise(exercise)
+    end
+  end
+
+  defp default_exercises do
+    [
+      "Bicycle Crunch",
+      "Dumbbell Lunge",
+      "Good Morning",
+      "Hip Abductor",
+      "Hip Adductor",
+      "Incline Dumbbell Press",
+      "Overhead Press",
+      "Pull-Up",
+      "Push-Up"
+    ]
   end
 end
