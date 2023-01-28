52bf924a47a06a95dba0e18ebd803b74af0f1e23
Nicholas <nskins@umich.edu>
2023-01-28 13:29:12 -0500

    Run 'mix format'

--- lib/we_lift_web/live/dashboard.ex
+++ lib/we_lift_web/live/dashboard.ex
@@ -1,14 +1,15 @@
 defmodule WeLiftWeb.DashboardLive do
   use WeLiftWeb, :live_view
 
-
   @impl true
   def render(assigns) do
     ~H"""
     <.header>Dashboard</.header>
     <div class="flex flex-col">
       <.link navigate={~p"/workouts"} class="text-blue-500 underline">Workouts</.link>
-      <.link navigate={~p"/exercises/history"} class="text-blue-500 underline">Exercise History</.link>
+      <.link navigate={~p"/exercises/history"} class="text-blue-500 underline">
+        Exercise History
+      </.link>
     </div>
     """
   end
@@ -17,5 +18,4 @@ defmodule WeLiftWeb.DashboardLive do
   def mount(_params, _session, socket) do
     {:ok, socket}
   end
- 
 end
