062441c0bac182c0975563e2bc3c5814a2d3db3b
Nicholas <nskins@umich.edu>
2023-01-29 20:00:53 -0500

    Update values of exercise_id and duration with input click events

--- lib/we_lift_web/live/exercises_live/history.ex
+++ lib/we_lift_web/live/exercises_live/history.ex
@@ -16,6 +16,17 @@ defmodule WeLiftWeb.ExerciseLive.History do
       errors={[]}
       type="select"
       options={@exercises}
+      phx-click="update_exercise_id"
+    />
+
+    <.input
+      id="duration_input"
+      name="duration_input"
+      value={@selected_duration}
+      errors={[]}
+      type="select"
+      options={@durations}
+      phx-click="update_duration"
     />
     """
   end
@@ -29,9 +40,42 @@ defmodule WeLiftWeb.ExerciseLive.History do
 
     {_, selected_exercise_id} = Enum.at(exercises, 0)
 
+    durations =
+      [
+        "One month": "1M",
+        "Three months": "3M",
+        "Six months": "6M",
+        "One year": "1Y"
+      ]
+
+    {_, selected_duration} = Enum.at(durations, 0)
+
     {:ok,
      socket
      |> assign(:exercises, exercises)
-     |> assign(:selected_exercise_id, selected_exercise_id)}
+     |> assign(:selected_exercise_id, selected_exercise_id)
+     |> assign(:durations, durations)
+     |> assign(:selected_duration, selected_duration)}
   end
+  
+  @impl true
+  def handle_event("update_exercise_id", %{"value" => new_exercise_value}, socket) do
+    
+   new_exercise_id = String.to_integer(new_exercise_value) 
+
+    case socket.assigns.selected_exercise_id do
+      ^new_exercise_id -> {:noreply, socket}
+      _ -> {:noreply, assign(socket, :selected_exercise_id, new_exercise_id)}
+    end
+  end
+
+  @impl true
+  def handle_event("update_duration", %{"value" => new_duration}, socket) do
+
+    case socket.assigns.selected_duration do
+      ^new_duration -> {:noreply, socket}
+      _ -> {:noreply, assign(socket, :selected_duration, new_duration)}
+    end
+  end
+
 end
