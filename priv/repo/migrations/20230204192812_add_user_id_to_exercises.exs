dbccc72aa03201f69cd471971afa48ac9a11d4d7
Nicholas <nskins@umich.edu>
2023-02-04 14:42:50 -0500

    Implement migration to permit an exercise to reference its creator

--- priv/repo/migrations/20230204192812_add_user_id_to_exercises.exs
+++ priv/repo/migrations/20230204192812_add_user_id_to_exercises.exs
@@ -2,6 +2,10 @@ defmodule WeLift.Repo.Migrations.AddUserIdToExercises do
   use Ecto.Migration
 
   def change do
+    alter table("exercises") do
+      add :user_id, references(:users)
+    end
 
+    create index(:exercises, [:user_id])
   end
 end
