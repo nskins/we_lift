1d8c2fecae7c8fc28770b762a3b9c77e541ab7b7
Nicholas <nskins@umich.edu>
2023-02-11 16:01:43 -0500

    Write repo function for creating a custom exercise

--- lib/we_lift/workouts.ex
+++ lib/we_lift/workouts.ex
@@ -9,6 +9,33 @@ defmodule WeLift.Workouts do
   alias WeLift.Workouts.Exercise
   alias WeLift.Workouts.Set
   alias WeLift.Workouts.Workout
+  
+  @doc """
+  Creates a user-specific exercise.
+
+  ## Examples
+
+    iex> create_exercise(%{id: 5}, %{name: "Power Clean"})
+    {:ok, %Exercise{}}
+
+    iex> create_exercise(%{id: 5}, %{name: ""})
+    {:error, %Ecto.Changeset{}}
+
+  """
+  def create_exercise(user, attrs \\ %{}) do
+    user_id = Integer.to_string(user.id)
+
+    IO.inspect(user_id)
+    IO.inspect(attrs["user_id"])
+
+    # This verifies that the User has permission to 
+    # create an Exercise with these parameters.
+    %{"user_id" => ^user_id} = attrs
+
+    %Exercise{}
+    |> Exercise.changeset(attrs)
+    |> Repo.insert()
+  end
 
   @doc """
   Creates a set.
