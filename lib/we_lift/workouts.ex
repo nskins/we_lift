22408ba49eef8025451263725daea35f9e6ed7fc
Nicholas <nskins@umich.edu>
2023-02-17 19:04:54 -0500

    Return history data as a map

--- lib/we_lift/workouts.ex
+++ lib/we_lift/workouts.ex
@@ -151,7 +151,7 @@ defmodule WeLift.Workouts do
           w.user_id == ^user_id and s.exercise_id == ^exercise_id and
             s.inserted_at > ago(^months_back, "month"),
         group_by: [fragment("?::date", s.inserted_at)],
-        select: {max(s.inserted_at), min(s.weight_in_lbs), max(s.weight_in_lbs)}
+        select: %{date: max(s.inserted_at), min: min(s.weight_in_lbs), max: max(s.weight_in_lbs)}
 
     Repo.all(query)
   end
