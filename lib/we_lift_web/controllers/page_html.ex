520eb3a11dd953670499e3e238ff135ef50b9c77
Nicholas <nskins@umich.edu>
2023-03-11 13:11:04 -0500

    Update remaining feature sections to use component

--- lib/we_lift_web/controllers/page_html.ex
+++ lib/we_lift_web/controllers/page_html.ex
@@ -5,19 +5,48 @@ defmodule WeLiftWeb.PageHTML do
 
   embed_templates "page_html/*"
 
+  attr :title, :string, required: true
+  attr :description, :string, required: true
+  attr :image, :string, required: true
+  attr :border, :boolean, default: false
+  attr :black, :boolean, default: false
+  attr :reverse, :boolean, default: false
+  attr :hero, :boolean, default: false
+
   def feature(assigns) do
+    assigns =
+      assigns
+      |> assign(:border_style, if(assigns.border, do: "border-2 border-black", else: ""))
+      |> assign(
+        :color_style,
+        if(assigns.black, do: "bg-black text-white", else: "bg-white text-black")
+      )
+      |> assign(
+        :image_order_style,
+        if(assigns.reverse, do: "lg:order-1 order-2", else: "order-2")
+      )
+      |> assign(:text_order_style, if(assigns.reverse, do: "lg:order-2 order-1", else: "order-1"))
+      |> assign(
+        :height_style,
+        if(assigns.hero, do: "lg:h-[680px] h-[780px]", else: "lg:h-[580px] h-[730px]")
+      )
+      |> assign(:title_font_size, if(assigns.hero, do: "text-6xl", else: "text-4xl"))
+      |> assign(:description_font_size, if(assigns.hero, do: "text-2xl", else: "text-xl"))
+
     ~H"""
-    <div class="flex lg:h-[580px] h-[730px] flex-row p-8">
+    <div class={"flex #{@height_style} flex-row p-8 #{@color_style}"}>
       <div class="flex lg:flex-row flex-col container mx-auto">
-        <div class="flex lg:w-1/2 p-8">
+        <div class={"flex lg:w-1/2 p-8 #{@text_order_style}"}>
           <div class="flex flex-col justify-center max-w-2xl">
-            <div class="font-bold lg:text-start text-center text-4xl"><%= @title %></div>
-            <div class="text-xl lg:text-start text-center"><%= @description %></div>
+            <div class={"font-bold lg:text-start text-center #{@title_font_size}"}><%= @title %></div>
+            <div class={"lg:text-start text-center #{@description_font_size}"}>
+              <%= @description %>
+            </div>
           </div>
         </div>
-        <div class="flex lg:w-1/2 justify-center">
+        <div class={"flex lg:w-1/2 justify-center #{@image_order_style}"}>
           <div class="flex flex-col justify-center max-w-xl">
-            <img class="border-2 border-black rounded-2xl" src={@image} />
+            <img class={"rounded-2xl #{@border_style}"} src={@image} />
           </div>
         </div>
       </div>
