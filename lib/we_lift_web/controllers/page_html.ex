20967017f1f59fee01c5fdef4c6d4ba10afd0ee1
Nicholas <nskins@umich.edu>
2023-03-11 14:09:44 -0500

    Update landing page (#49)
    * Move existing landing page elements to page header
    
    * Bold the app name in the homepage header
    
    * Start working on hero element for landing page
    
    * Add image of exercise history to hero element on landing page
    
    * Add some transition sentences after the hero element
    
    * Add centered container to the page header
    
    * Add a container within the hero element and adjust styles
    
    * Add container to transition and fix text centering
    
    * Add the first feature section to landing page
    
    * Add accurate image for first feature
    
    * Run 'mix format'
    
    * Add the second feature section to landing page
    
    * Round the corners of the history image
    
    * Add section on the FOSS nature of the project
    
    * Make the height of each section more consistent
    
    * Switch to flex column orientation for small width screens
    
    * Make the hero element responsive to small width screens
    
    * Make the second feature section responsive to small width screens
    
    * Make the FOSS section responsive to small width screens
    
    * Always center the hero element image
    
    * Reverse the order with order-* Tailwind classes to prevent justify to bottom
    
    * Run 'mix format'
    
    * Add feature component and update first component on landing page
    
    * Update remaining feature sections to use component
    
    * Link to homepage upon clicking app name
    
    * Add link to source code back
    
    * Run 'mix format'

--- lib/we_lift_web/controllers/page_html.ex
+++ lib/we_lift_web/controllers/page_html.ex
@@ -1,5 +1,65 @@
 defmodule WeLiftWeb.PageHTML do
   use WeLiftWeb, :html
 
+  use Phoenix.Component
+
   embed_templates "page_html/*"
+
+  attr :title, :string, required: true
+  attr :description, :string, required: true
+  attr :image, :string, required: true
+  attr :border, :boolean, default: false
+  attr :black, :boolean, default: false
+  attr :reverse, :boolean, default: false
+  attr :hero, :boolean, default: false
+  attr :link, :boolean, default: false
+
+  def feature(assigns) do
+    assigns =
+      assigns
+      |> assign(:border_style, if(assigns.border, do: "border-2 border-black", else: ""))
+      |> assign(
+        :color_style,
+        if(assigns.black, do: "bg-black text-white", else: "bg-white text-black")
+      )
+      |> assign(
+        :image_order_style,
+        if(assigns.reverse, do: "lg:order-1 order-2", else: "order-2")
+      )
+      |> assign(:text_order_style, if(assigns.reverse, do: "lg:order-2 order-1", else: "order-1"))
+      |> assign(
+        :height_style,
+        if(assigns.hero, do: "lg:h-[680px] h-[780px]", else: "lg:h-[580px] h-[730px]")
+      )
+      |> assign(:title_font_size, if(assigns.hero, do: "text-6xl", else: "text-4xl"))
+      |> assign(:description_font_size, if(assigns.hero, do: "text-2xl", else: "text-xl"))
+
+    ~H"""
+    <div class={"flex #{@height_style} flex-row p-8 #{@color_style}"}>
+      <div class="flex lg:flex-row flex-col container mx-auto">
+        <div class={"flex lg:w-1/2 p-8 #{@text_order_style}"}>
+          <div class="flex flex-col justify-center max-w-2xl">
+            <div class={"font-bold lg:text-start text-center #{@title_font_size}"}><%= @title %></div>
+            <div class={"lg:text-start text-center #{@description_font_size}"}>
+              <%= @description %>
+            </div>
+            <%= if @link do %>
+              <a
+                class="lg:text-start text-center text-blue-700 underline text-xl"
+                href="https://github.com/nskins/we_lift"
+              >
+                View the source code
+              </a>
+            <% end %>
+          </div>
+        </div>
+        <div class={"flex lg:w-1/2 justify-center #{@image_order_style}"}>
+          <div class="flex flex-col justify-center max-w-xl">
+            <img class={"rounded-2xl #{@border_style}"} src={@image} />
+          </div>
+        </div>
+      </div>
+    </div>
+    """
+  end
 end
