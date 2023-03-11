f1f4adbd6302aab85118838b654f2916593b3295
Nicholas <nskins@umich.edu>
2023-03-11 12:12:28 -0500

    Add feature component and update first component on landing page

--- lib/we_lift_web/controllers/page_html.ex
+++ lib/we_lift_web/controllers/page_html.ex
@@ -1,5 +1,27 @@
 defmodule WeLiftWeb.PageHTML do
   use WeLiftWeb, :html
 
+  use Phoenix.Component
+
   embed_templates "page_html/*"
+
+  def feature(assigns) do
+    ~H"""
+    <div class="flex lg:h-[580px] h-[730px] flex-row p-8">
+      <div class="flex lg:flex-row flex-col container mx-auto">
+        <div class="flex lg:w-1/2 p-8">
+          <div class="flex flex-col justify-center max-w-2xl">
+            <div class="font-bold lg:text-start text-center text-4xl"><%= @title %></div>
+            <div class="text-xl lg:text-start text-center"><%= @description %></div>
+          </div>
+        </div>
+        <div class="flex lg:w-1/2 justify-center">
+          <div class="flex flex-col justify-center max-w-xl">
+            <img class="border-2 border-black rounded-2xl" src={@image} />
+          </div>
+        </div>
+      </div>
+    </div>
+    """
+  end
 end
